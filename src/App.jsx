ed7f08d038f148896d009aa34046c6ce125532b7
Froxayn <rizkymyusuf123@gmail.com>
2024-05-06 12:35:36 +0700

    fix: Configure React-Router-DOM

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import NotFound from "./pages/NotFound";
+
 const App = () => {
   return (
     <>
-      <header></header>
-      <main></main>
-      <footer></footer>
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="*" element={<NotFound />}></Route>
+      </Routes>
     </>
   );
 };
