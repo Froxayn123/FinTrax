7aa1e332e9e466b3ad87f206e1066498fbd09c1e
Froxayn <rizkymyusuf123@gmail.com>
2024-05-09 08:06:26 +0700

    feat: Adding Animation on LoginButton

--- src/assets/LoginButton.jsx
+++ src/assets/LoginButton.jsx
@@ -1,7 +1,9 @@
 const LoginButton = () => {
   return (
     <>
-      <button className="px-[24px] py-[10px] rounded-[8px] font-roboto text-[22px] capitalize border border-[#ECEFF5] text-[#ECEFF5]">login</button>
+      <button className="px-[24px] py-[10px] rounded-[8px] font-roboto text-[22px] capitalize border border-[#ECEFF5] text-[#ECEFF5] hover:bg-[#6750A4] hover:border-[#6750A4] hover:drop-shadow-[0px_3px_3px_rgba(0,0,0,0.75)] transition-all ease-in-out duration-300">
+        login
+      </button>
     </>
   );
 };
