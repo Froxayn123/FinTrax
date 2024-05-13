3c3111def0a6c0c6244eef11725f066f3d552254
Froxayn <rizkymyusuf123@gmail.com>
2024-05-13 16:42:29 +0700

    fix: fix App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import { Routes, Route } from "react-router-dom";
 import NotFound from "./pages/NotFound";
 import LandingPage from "./pages/LandingPage";
+import Login from "./pages/Login";
+import Verifikasi from "./pages/VerifikasiAkun";
 
 const App = () => {
   return (
     <>
       <Routes>
         <Route path="/" element={<LandingPage />}></Route>
+        <Route path="/Login" element={<Login />}></Route>
+        <Route path="/Verifikasi" element={<Verifikasi />}></Route>
         <Route path="*" element={<NotFound />}></Route>
       </Routes>
     </>
