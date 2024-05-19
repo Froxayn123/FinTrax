47b782c66fa3146fff2152967c22adf52c6c3bbc
SalehMall <kemalbadawi@gmail.com>
2024-05-19 20:20:19 +0800

    add tampilan Overview

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,13 @@ import LandingPage from "./pages/LandingPage";
 import Login from "./pages/Login";
 import Verifikasi from "./pages/VerifikasiAkun";
 import Register from "./pages/Register";
+import Dashboard_akun from "./pages/Dashboard_akun";
 
 const App = () => {
   return (
     <>
       <Routes>
+        <Route path="Dashboard" element={<Dashboard_akun/>}></Route>
         <Route path="/" element={<LandingPage />}></Route>
         <Route path="/login" element={<Login />}></Route>
         <Route path="/Register" element={<Register />}></Route>
