f6661547edee4b2efd8a7628cabb5e0cfc3fa051
Froxayn <rizkymyusuf123@gmail.com>
2024-05-21 17:25:17 +0700

    fix: position on Polygon5.jsx & Arrow.jsx

--- src/assets/Polygon5.jsx
+++ src/assets/Polygon5.jsx
@@ -1,7 +1,7 @@
 const Polygon5 = () => {
   return (
     <>
-      <div className="absolute -bottom-[1900px] start-96 -z-50">
+      <div className="absolute -bottom-[1750px] start-[600px] -z-50">
         <svg xmlns="http://www.w3.org/2000/svg" width="249" height="259" viewBox="0 0 249 259" fill="none">
           <g filter="url(#filter0_f_242_26780)">
             <path d="M6.09166 115.239C32.5881 -2.58402 182.617 -38.9581 233.039 60.2159C270.446 133.791 221.528 232.957 137.816 253.252C54.1033 273.548 -13.5654 202.649 6.09166 115.239Z" stroke="#713B8B" strokeWidth="0.5" />
