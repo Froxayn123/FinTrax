2eeadb4e0aed5bb53345bc72d942ffd14128b7d1
Froxayn <rizkymyusuf123@gmail.com>
2024-05-21 17:48:59 +0700

    fix: button on ArticleSection.jsx

--- src/components/ArticlesSection.jsx
+++ src/components/ArticlesSection.jsx
@@ -9,7 +9,7 @@ const ArticlesSection = () => {
         <div className="space-y-[24px]">
           <h1 className="w-[590px] text-gray-200 text-[40px] font-bold font-['Roboto'] leading-[48px]">Financial Tips & Articles</h1>
           <p className="w-[590px] text-gray-200 text-[22px] font-medium font-['Roboto'] leading-7">We offer a variety of articles and tips to help you improve your financial literacy.</p>
-          <Button title="Explore More" />
+          <Button title="Explore More" colors={"bg-gradient-primary"} />
         </div>
         <div className="grid grid-rows-2 gap-4">
           {articles.map((article, i) => {
