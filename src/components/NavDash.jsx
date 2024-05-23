400bb2a8c6dac40e99e50ff4ec0622914dd0fd93
Froxayn <rizkymyusuf123@gmail.com>
2024-05-23 10:50:12 +0700

    fix: Navbar Dashboard

--- src/components/NavDash.jsx
+++ src/components/NavDash.jsx
@@ -3,7 +3,14 @@ import PropTypes from "prop-types";
 
 const NavDash = ({ title, icon, to }) => {
   return (
-    <Link to={to} className="w-[191px] text-white group flex items-center space-x-2 py-2 px-3 rounded-lg transition duration-300 ease-in-out hover:bg-white hover:text-[#001833]">
+    <Link
+      to={to}
+      className={
+        window.location.pathname == to
+          ? "w-[191px] group flex items-center space-x-2 py-2 px-3 rounded-lg transition duration-300 ease-in-out bg-white text-[#001833]"
+          : "w-[191px] group text-white flex items-center space-x-2 py-2 px-3 rounded-lg transition duration-300 ease-in-out hover:bg-white hover:text-[#001833]"
+      }
+    >
       {icon}
       <span className="text-[22px] font-medium font-['Roboto']">{title}</span>
     </Link>
