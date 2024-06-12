06f85c0d2426da56c2ea6ed7d9bcf496b1b9298a
Adhityaekp <114294208+Adhityaekp@users.noreply.github.com>
2024-06-13 06:22:11 +0700

    update

--- src/components/FooterContent.jsx
+++ src/components/FooterContent.jsx
@@ -10,32 +10,32 @@ import FooterDataCenter from "../assets/FooterDataCenter";
 const FooterContent = () => {
   return (
     <>
-      <section className="absolute -top-16 w-full flex flex-col justify-center items-center space-y-[64px]">
-        <div className=" w-full flex justify-between items-start">
-          <div className="flex flex-col justify-start items-start space-y-4">
+      <section className="absolute -top-20  w-full flex flex-col justify-center items-center space-y-[64px]">
+        <div className="w-full flex flex-col md:flex-row justify-between items-center md:items-start space-y-8 md:space-y-0 md:space-x-[64px]">
+          <div className="flex flex-col justify-start items-center md:items-start space-y-4">
             <img src="/LogoFooter.png" alt="Logo.png" className="w-[200px] h-[80px]" />
-            <p className="w-[200px] text-white text-base font-normal font-['Roboto'] leading-normal tracking-wide text-justify">
+            <p className="hidden md:block w-[200px] text-white text-base font-normal font-['Roboto'] leading-normal tracking-wide text-center md:text-justify">
               Fintrax helps you track your income and expenses, plan your budget, and achieve your financial goals with ease and confidence.
             </p>
           </div>
-          <div className="flex justify-between items-start space-x-[64px]">
-            {FooterDatas.head.map((data, i) => {
-              return <FooterDataCenter key={i} name={data.name} link={data.link} />;
-            })}
+          <div className="flex justify-between items-center md:items-start space-x-[64px]">
+            {FooterDatas.head.map((data, i) => (
+              <FooterDataCenter key={i} name={data.name} link={data.link} />
+            ))}
           </div>
-          <div className="flex flex-col justify-start items-start space-y-[16px]">
+          <div className="flex flex-col justify-center md:justify-start items-center md:items-start space-y-[16px]">
             <div className="flex justify-center items-center space-x-[24px]">
               <FBIcon />
               <TeleIcon />
               <IGIcon />
               <LNIcon />
             </div>
-            <p className="w-52 text-white text-sm font-normal font-['Roboto'] leading-tight tracking-tight">Follow Us for Financial Tips and More</p>
+            <p className="w-52 text-white text-center md:text-start text-sm font-normal font-['Roboto'] leading-tight tracking-tight">Follow Us for Financial Tips and More</p>
           </div>
         </div>
         <div className="flex justify-center items-center space-x-2">
           <p className="text-gray-100 text-[16.80px] font-normal font-['Quicksand']">Copyright 2024</p>
-          <CopyRight />
+          <CopyRight/>
           <p className="text-gray-100 text-[16.80px] font-normal font-['Quicksand']">Fintrax</p>
         </div>
       </section>
