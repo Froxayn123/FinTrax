6de53838aaffe4498023548291856601a1293183
Adhityaekp <114294208+Adhityaekp@users.noreply.github.com>
2024-06-13 06:28:52 +0700

    update

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,8 +3,8 @@ import FooterContent from "./FooterContent";
 const Footer = () => {
   return (
     <>
-      <div className="">
-        <svg className="h-[800px] md:h-auto " xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 600" fill="none">
+      <div>
+        <svg className="h-[800px] md:h-auto" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 600" fill="none">
           <path d="M1439.1 600.004H-0.779652C-0.779652 544.097 -0.77975 202.021 -0.903809 143.438C197.666 167.748 356.185 186.044 590.929 143.438C1009.29 67.5069 1439.1 0.00390625 1439.1 0.00390625V600.004Z" fill="#0D243D" />
         </svg>
       </div>
